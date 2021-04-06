431fae5b7895569090cf189ad6a7a7035a9d3e32
Abanoub Farag <AbanoubMarcos.Farag@calbaptist.edu>
2021-04-06 13:57:50 -0700

    finished I think

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    const dbURI = "mongodb+srv://abanoub:Kyrillos17@cluster0.mongodb.net/ecommerce?retryWrites=true&w=majority";
+    const dbURI = "mongodb+srv://abanoub:loser@cluster0.mongodb.net/ecommerce?retryWrites=true&w=majority";
     mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(result => console.log("connected to MongoDB Atlas"))
         .catch(err => console.log(err));
